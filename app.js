479f82052d8601fef3fbc5ff42f29457cf0d6d3c
Meka <mekatroniko@gmail.com>
2016-08-30 21:46:02 -0500

    setting up all and using socket.io

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var express = require('express'),
+var app = require('express')(),
+	http = require('http').Server(app),
+	io = require('socket.io')(http),
    path = require('path');
-
-var app = express();
-//var io = require('socket.io').listen(app);
 
 app.get('/', function (req, res) {
 	var ruta = path.join(__dirname, 'index.html');
   	res.sendFile(ruta);
 });
 
-app.listen(8008);
-/*io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function (socket) {
   socket.on('turn played', function(data){
     socket.broadcast.emit('turn played', data);
   });
   socket.on('winner', function(data){
     socket.broadcast.emit('game over', { "winner": data });
   });
-});*/
+});
+
+http.listen(8008, function(){
+	console.log('Running on *:8008');
+})
